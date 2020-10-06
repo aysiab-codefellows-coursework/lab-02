ca42edd69e3ba748a625ecd34c5101a29c81dfa8
Aysia Brown <aysiaebrown@gmail.com>
2020-10-06 14:53:11 -0700

    prints images from json file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# lab-02
-Lab 02 JQuery Selectors 
+# Lab-02 JQuery Selectors Image Gallery
+## Aysia Brown & Ryan Pilon
+
+### Feature #1: Display Images
+*Estimated Time To Complete:* 1hr
+*Start Time:* 2:30PM
+*Finish Time:* 2:50PM
+*Actual Elapsed Time:* 20 minutes
+
+### Feature #2: Filter Images
+*Estimated Time To Complete:* 2hr
+*Start Time:*
+*Finish Time:*
+*Actual Elapsed Time:*
+
+### Feature #3: Style Application
+*Estimated Time To Complete:* 1.5hr
+*Start Time:*
+*Finish Time:*
+*Actual Elapsed Time:*
+
+
+### References:
+- we referenced the demo code from lecture. 
--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+function HornedCritter(critter) {
+    this.image = critter.image_url;
+    this.title = critter.title;
+    this.desc = critter.description
+    this.tag = critter.keyword;
+    this.horns = critter.horns;
+}
+
+HornedCritter.prototype.render = function() {
+    let $galleryClone = $('.gallery-temp').clone();
+    $('main').append($galleryClone);
+
+    $galleryClone.find('h2').text(this.title);
+    $galleryClone.find('img').attr('src', this.image);
+    $galleryClone.find('p').text(this.desc);
+    $galleryClone.removeClass('gallery-temp');
+    $galleryClone.attr('class', this.title);
+}
+
+HornedCritter.readJSON = () => {
+    const ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    }
+
+    $.ajax('../data/page-1.json', ajaxSettings)
+        .then(data => {
+            data.forEach( item => {
+                let critter = new HornedCritter(item);
+                critter.render();
+            })
+        })
+}
+
+$(() => HornedCritter.readJSON());
